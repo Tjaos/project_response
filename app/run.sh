a241ac7d072910e9633791a67e2b496a794d7910
tjaos <th83287716@gmail.com>
2024-03-23 18:18:36 -0300

    feat(shell):automating project startup with shell

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+# Instalar as dependências do frontend
+cd frontend
+npm install
+cd ..
+
+# Instalar as dependências do backend
+cd api
+pip install flask flask-cors unidecode
+
+# Iniciar o backend
+python app.py &
+sleep 5
+cd ..
+
+# Iniciar o frontend
+cd frontend
+npm run dev
+sleep 10
